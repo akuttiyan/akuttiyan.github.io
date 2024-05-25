829c1da4117ce11c207866038f8531b3db517330
akuttiyan <akuttiyan@eastsideprep.org>
2024-05-24 18:14:58 -0700

    added stuff to the wbesite

--- cs-in-healthcare/code/dna_assembly.py
+++ cs-in-healthcare/code/dna_assembly.py
@@ -12,7 +12,7 @@ def split_sequence(dna_sequence):
     split_sequence = []
     num_splits = len(dna_sequence) * 4
 
-    print("num splits", num_splits)
+    #print("num splits", num_splits)
     for i in range(int(num_splits)):
         #print(i)
 
@@ -24,7 +24,7 @@ def split_sequence(dna_sequence):
             start = random.randrange(0, len(dna_sequence) - 1)
             end = random.randrange(start, len(dna_sequence))
 
-        split_piece = dna_sequence[start : end]
+        split_piece = dna_sequence[start : end + 1]
         #print(split_piece)
         split_sequence.append(split_piece)
 
@@ -82,7 +82,7 @@ def find_similarities(dna_1, dna_2):
     j = 0
     f = 0
     k = 1
-    index = []
+    index = dict()
     
 
     #print("test1", dna_1)
@@ -115,21 +115,36 @@ def find_similarities(dna_1, dna_2):
             if child[i : i + 3] == parent[j : j + 3]:
                 #print("testing")
                 
-                index = [parent, child, [i, i + 2], [j, j + 2]]
+                #index = [parent, child, [i, i + 2], [j, j + 2]]
+                if dna_1 == parent:
+                    index[dna_1] = [j, j + 2]
+                    index[dna_2] = [i, i + 2]
+                    #index = [dna_1, dna_2, [i, i + 2], [j, j + 2]]
+                elif dna_2 == parent:
+                    index[dna_1] = [i, i + 2]
+                    index[dna_2] = [j, j + 2]
+                    #index = [dna_1, dna_2, [i, i + 2], [j, j + 2]]
                 #print(index)
                 for f in range(i + 3, len(child)):
                     #print("child", child[i : f + 1])
                     #print("parensf", parent[j : j + 3 + k])
                     if child[i : f + 1] == parent[j : j + 3 + k]:
-                        index = [parent, child, [i, f], [j, j + 2 + k]]
+                        if dna_1 == parent:
+                            #index[dna_1] = [j, j + 3 + k]
+                            index[dna_1] = [j, (j + 3 + k) - 1]
+                            index[dna_2] = [i, f]
+                        elif dna_2 == parent:
+                            index[dna_1] = [i, f]
+                            index[dna_2] = [j, (j + 3 + k) - 1]
+                            #index[dna_2] = [j, j + 3 + k]
                         #return index
                     k += 1
                 #print("yes")
                 #print("index", index)
                 return index
 
-#test_dna = "CTGAAA"
-#other_dna = "CTGAA"
+#test_dna = "ATCTGAATGTAT"
+#other_dna = "CTGAATGT"
 
 #similarities = find_similarities(test_dna, other_dna)
 #print("function test", similarities)
@@ -145,9 +160,9 @@ def find_similarities(dna_1, dna_2):
 
 def find_overlap(dna_split_sequence):
     i = 0
-    over_lap_dict = defaultdict(list)
+    #over_lap_dict = defaultdict(list)
     dna_groups = []
-    overlap = dict()
+    overlaps = dict()
     j = 0
 
     #print("test length", len(dna_split_sequence))
@@ -158,10 +173,10 @@ def find_overlap(dna_split_sequence):
 
             if split != dna_split_sequence[i]:
                 test_dna = dna_split_sequence[i]
-                print("test_dna", test_dna)
-                print("split", split)
+                #print("test_dna", test_dna)
+                #print("split", split)
                 overlap_info = find_similarities(test_dna, split)
-                print("test1", overlap_info)
+                #print("overlap info", overlap_info)
                 if overlap_info is not None:
       
                     #print("overlap info" ,  overlap_info)
@@ -170,21 +185,27 @@ def find_overlap(dna_split_sequence):
                     #over_lap_dict[dna_split_sequence[i][0]].append([overlap_info[1], overlap_info[2], overlap_info[3]])
                     #else:
                     #print(dna_split_sequence[i])
+                    #try:
+                    #    overlaps[dna_split_sequence[i]].append([overlap_info[1], overlap_info[2], overlap_info[3]])
+                    #except:
+                    #    overlaps[dna_split_sequence[i]] = [overlap_info[0], overlap_info[2], overlap_info[3]]
                     try:
-                        overlap[dna_split_sequence[i]].append([overlap_info[1], overlap_info[2], overlap_info[3]])
+                        overlaps[test_dna].append(split, overlap_info[test_dna], overlap_info[split])
                     except:
-                        overlap[dna_split_sequence[i]] = [overlap_info[1], overlap_info[2], overlap_info[3]]
+                        overlaps[test_dna] = [[split, overlap_info[test_dna], overlap_info[split]]]
+                        break
+                        #print(overlaps.items())
                     
                     #over_lap_dict[dna_split_sequence[i]].append([overlap_info[1], overlap_info[2], overlap_info[3]])
                     #print(dna_split_sequence[i])
     
-    return overlap
+    return overlaps
 
-test_split_sequence = ["ACTGG", "TGGC", "GGCA"]
+#test_split_sequence = ["ACTGG", "TGGC", "GGCA"]
 
-overlap = find_overlap(test_split_sequence)
+#overlap = find_overlap(test_split_sequence)
 
-print("functioon test", overlap)
+#print("functioon test", overlap)
 
 '''
     test_dna = dna_split_sequence.pop(0)
@@ -205,7 +226,7 @@ print("functioon test", overlap)
     return over_lap_dict
     '''
 
-
+#dna_split_sequence = ["ATCG", "GCCC", "TCGAAA", "CCCTA"]
                 
 #dictionary = find_overlap(dna_split_sequence)
 #items = dictionary.items()
@@ -227,7 +248,8 @@ def combine_splits(over_lap_dict, key):
     combined_split = key_list[0]
 
     #print("combined split", combined_split[0])
-    print(over_lap_dict)
+    #print("over lap dict", over_lap_dict)
+    '''
     for original_value in over_lap_dict:   
         for value in over_lap_dict[original_value]:
         # print("new value", value)
@@ -237,26 +259,28 @@ def combine_splits(over_lap_dict, key):
             
             #print("val", len(value[0]))
             #print("value", value[0])
+            print("original value", original_value)
+            print(value)
             if combined_split in value[0]:
                 combined_split = value[0]
-                print("value", value[0][0 : splice])
+                #print("value", value[0][0 : splice])
                     
             elif value[0] in combined_split:
                 print("do nothing")
                 #print(combined_split)
-                print("value", value[0][0 : splice])
+                #print("value", value[0][0 : splice])
             elif (value[1][1] + 1) < len(value[0]):
                 splice = len(value[0]) - value[1][1] - 1
                 combined_split = combined_split + value[0][splice : len(value[0])]
                 #print("test", combined_split[0])
-                print("value", value[0][0 : splice])
+                #print("value", value[0][0 : splice])
             elif value[1][0] > 0 and value[2][0] == 0:
                 #print("yes")
                 splice = value[1][0] - value[2][0]
                 
                 #print("overlap", value[0][0 : splice])
                 #print(combined_split[0])
-                print("value", value[0][0 : splice])
+                #print("value", value[0][0 : splice])
                 combined_split = value[0][0 : splice] + combined_split
                 
 
@@ -264,16 +288,63 @@ def combine_splits(over_lap_dict, key):
             else:
                 print("unknown case??")
             print("unknown", value, combined_split)
+    '''
+    
+    for value in over_lap_dict[key]: 
+
+    # print("new value", value)
+
+
+        #print(combined_split[0])
+            
+        #print("val", len(value[0]))
+        #print("original values", over_lap_dict[key])
+        #print("value", value)
+        dna_string = value[0]
+        dna_string_index_2 = value[2][1]
+        #print(dna_string_index_2)
+        dna_string_index_1 = value[2][0]
+        other_dna_string_index_1 = value[1][0]
+        #print(dna_string)
+        if combined_split in dna_string:
+            combined_split = dna_string
+                #print("value", value[0][0 : splice])
+                    
+        elif dna_string in combined_split:
+            print("do nothing")
+                #print(combined_split)
+                #print("value", value[0][0 : splice])
+        elif (dna_string_index_2 + 1) < len(dna_string):
+            #splice = len(dna_string) - dna_string_index_2 + 1 #- 1
+            splice = dna_string_index_2 + 1 
+            combined_split = combined_split + dna_string[splice : len(dna_string)]
+                #print("test", combined_split[0])
+                #print("value", value[0][0 : splice])
+        elif dna_string_index_1 > 0 and other_dna_string_index_1 == 0:
+                #print("yes")
+            #splice = other_dna_string_index_1 - dna_string_index_1
+            splice = dna_string_index_1
+                
+                #print("overlap", value[0][0 : splice])
+                #print(combined_split[0])
+                #print("value", value[0][0 : splice])
+            combined_split = dna_string[0 : splice] + combined_split
+                
+
+            j +=1
+        else:
+            print("unknown case??")
+        #print("unknown", combined_split)
     return combined_split
 
 
 #overlaps = dict()
 #overlaps["CTAGAT"] = [["GATCCC", [0, 2], [3, 5]], ["GAT", [0, 2], [3, 5]], ["CTACTA", [3, 5], [0, 2]], ["TAGAT", [0, 4], [1, 5]], ["GACTACCCCCTA", [9, 11], [0, 2]]]
 
-#test = combine_splits(overlaps, "CTAGAT")
+#test = combine_splits(dictionary, "CCCTA")
 
 #print("function test", test)
-
+#print("overlaps", overlaps)
 
 
 
@@ -334,12 +405,13 @@ print(parents)
 '''
 
 
-'''
-#dna = "ATGCC"
+dna = "TAATCGAGGCGAG"
 
-#dna_split_sequence = split_sequence(dna)
+dna_split_sequence = split_sequence(dna)
 
-dna_split_sequence = ["ATGC", "ATG", "GCC", "ATGC"]
+print(dna_split_sequence)
+
+#dna_split_sequence = ["ATGC", "TGCCG", "GCC", "ATGC"]
 
 #print(dna_split_sequence)
 
@@ -348,6 +420,7 @@ over_lap_dict = find_overlap(dna_split_sequence)
 dna_split_sequence = []
 final_sequence = ["a", "b"]
 
+'''
 #print("length", len(final_sequence))
 #print(over_lap_dict.items())
 while len(final_sequence) != 1:
@@ -365,25 +438,41 @@ while len(final_sequence) != 1:
     #print(over_lap_dict.items())
     
     dna_split_sequence = []
-
-
 '''
+
   
 
-'''
+
 while len(dna_split_sequence) != 1:
     dna_split_sequence = []
     for key in over_lap_dict.keys():
 
-        dna_split_sequence.append(combine_splits(over_lap_dict[key], key))
-        print("dna_split_sequence", dna_split_sequence)
+        dna_split_sequence.append(combine_splits(over_lap_dict, key))
+        
+        #print("dna_split_sequence", dna_split_sequence)
+    
+
     final_sequence = dna_split_sequence
+    #print(set(final_sequence))
+    
+    if len(set(final_sequence)) == 1: # studytonight.com
+        final_sequence = final_sequence[0]
+        print(final_sequence)
+        break
+
+    #equal = all(final_sequence)
+    #if equal:
+    #    print(final_sequence)
+    #    break
+
     #print("second", dna_split_sequence)
     over_lap_dict = find_overlap(final_sequence)
-    print(over_lap_dict.items())
+    #print(over_lap_dict)
+    #break
+    #print(over_lap_dict.items())
+print(final_sequence)
 
-print("final", final_sequence)
 
-'''
+
 
 
